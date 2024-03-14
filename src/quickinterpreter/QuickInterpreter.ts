796da5ad8747f376552042662401a1ac992a7f9e
MarkusEllyton <markusellyton@gmail.com>
2024-03-14 01:32:19 +0100

    Replace terminal profile with shell task, forgotten file

--- src/quickinterpreter/QuickInterpreter.ts
+++ src/quickinterpreter/QuickInterpreter.ts
@@ -6,27 +6,7 @@ export class QuickInterpreter extends AutoDisposable {
 
     constructor() {
         super();
-        this._registerTerminalProfileProvider();
-    }
-
-    private _registerTerminalProfileProvider() {
-        const _disp = commands.registerCommand("vdm-vscode.run-quick-interpreter", this.runQuickInterpreter);
-        // const _disp = window.registerTerminalProfileProvider("vdm-vscode.quick-interpreter-profile", {
-        //     provideTerminalProfile() {
-        //         return {
-        //             options: {
-        //                 name: "VDM Quick Interpreter",
-        //                 shellPath: "bash",
-        //                 shellArgs: [
-        //                     "-c",
-        //                     "java -Xmx2g -cp /home/mark_el/.vscode/extensions/overturetool.vdm-vscode-1.3.7/resources/jars/vdmj/vdmj-4.5.0-SNAPSHOT.jar VDMJ -i",
-        //                 ],
-        //             },
-        //         };
-        //     },
-        // });
-
-        this._disposables.push(_disp);
+        this._disposables.push(commands.registerCommand("vdm-vscode.run-quick-interpreter", this.runQuickInterpreter));
     }
 
     private runQuickInterpreter() {
