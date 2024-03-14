28e501eeaaf4e6f46e13fef658e4b39ddaacd77c
MarkusEllyton <markusellyton@gmail.com>
2024-03-14 01:31:30 +0100

    Replace terminal profile with shell task

--- src/quickinterpreter/QuickInterpreter.ts
+++ src/quickinterpreter/QuickInterpreter.ts
@@ -1,4 +1,4 @@
-import { window } from "vscode";
+import { ShellExecution, Task, TaskScope, commands, tasks, window, workspace } from "vscode";
 import AutoDisposable from "../helper/AutoDisposable";
 
 export class QuickInterpreter extends AutoDisposable {
@@ -10,21 +10,38 @@ export class QuickInterpreter extends AutoDisposable {
     }
 
     private _registerTerminalProfileProvider() {
-        const _disp = window.registerTerminalProfileProvider("vdm-vscode.quick-interpreter-profile", {
-            provideTerminalProfile() {
-                return {
-                    options: {
-                        name: "VDM Quick Interpreter",
-                        shellPath: "bash",
-                        shellArgs: [
-                            "-c",
-                            "java -Xmx2g -cp /home/mark_el/.vscode/extensions/overturetool.vdm-vscode-1.3.7/resources/jars/vdmj/vdmj-4.5.0-SNAPSHOT.jar VDMJ -i",
-                        ],
-                    },
-                };
-            },
-        });
+        const _disp = commands.registerCommand("vdm-vscode.run-quick-interpreter", this.runQuickInterpreter);
+        // const _disp = window.registerTerminalProfileProvider("vdm-vscode.quick-interpreter-profile", {
+        //     provideTerminalProfile() {
+        //         return {
+        //             options: {
+        //                 name: "VDM Quick Interpreter",
+        //                 shellPath: "bash",
+        //                 shellArgs: [
+        //                     "-c",
+        //                     "java -Xmx2g -cp /home/mark_el/.vscode/extensions/overturetool.vdm-vscode-1.3.7/resources/jars/vdmj/vdmj-4.5.0-SNAPSHOT.jar VDMJ -i",
+        //                 ],
+        //             },
+        //         };
+        //     },
+        // });
 
         this._disposables.push(_disp);
+    }
+
+    private runQuickInterpreter() {
+        const qiTask: Task = new Task(
+            {
+                type: "shell",
+            },
+            TaskScope.Workspace,
+            "VDM: Quick Interpreter",
+            "name",
+            new ShellExecution(
+                "java -Xmx2g -cp /home/mark_el/Documents/Programming/VDM/vdm-vscode-fork/resources/jars/vdmj/vdmj-4.5.0-SNAPSHOT.jar VDMJ -i"
+            )
+        );
+
+        tasks.executeTask(qiTask);
     }
 }
